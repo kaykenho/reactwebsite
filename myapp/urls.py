347f0821d87a41b1b4237cb07c5cf45a0a0c140c
kaykenho <kaykymatosf@gmail.com>
2024-08-20 12:32:32 -0300

    Initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,7 @@
+# myapp/urls.py
+from django.urls import path
+from .views import DataListCreate
+
+urlpatterns = [
+    path('data/', DataListCreate.as_view(), name='data-list-create'),
+]
---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Data
+from .serializers import DataSerializer
+
+class DataListCreate(generics.ListCreateAPIView):
+    queryset = Data.objects.all()
+    serializer_class = DataSerializer
+
+
